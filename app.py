6336438329e397e1a0f219507123b9ffd8e395d2
lusiluvitasari <21463325-lusiluvitasari@users.noreply.replit.com>
2023-05-16 07:17:53 +0000

    percobaan pertama

--- app.py
+++ app.py
@@ -5,19 +5,16 @@ import dash_html_components as html
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
-  html.H1("Selamat Datang di Aplikasi Web Sederhana"),
+  html.H1("Selamat Datang di Aplikasi Web Sederhan"),
   dcc.Input(id="input-text", type="text", placeholder="Masukkan teks"),
   html.Button("Kirim", id="submit-button", n_clicks=0),
   html.Div(id="output")
 ])
 
 
-@app.callback(
-  dash.dependencies.Output("output", "childern"),
-  [dash.dependencies.Input("submit-button", "n_clicks")],
-  [dash.dependencies.State("input-text", "value")]
-)
-
+@app.callback(dash.dependencies.Output("output", "childern"),
+              [dash.dependencies.Input("submit-button", "n_clicks")],
+              [dash.dependencies.State("input-text", "value")])
 def update_output(n_clicks, input_text):
   if n_clicks > 0 and input_text:
     return html.H2(f"Anda mengirim: {input_text}")
